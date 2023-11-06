9f9b85e7d897f1911137d11abd0b1109725f7754
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2023-11-06 06:27:33 +0530

    Update _AlwaysOnline.js

--- plugins/_AlwaysOnline.js
+++ plugins/_AlwaysOnline.js
@@ -3,9 +3,9 @@ export async function before(m) {
     if (!chat.autotype) return;
   
     const commands = Object.values(global.plugins).flatMap((plugin) => [].concat(plugin.command));
-    const presenceStatus = commands.some((cmd) => (cmd instanceof RegExp ? cmd.test(m.text) : m.text.includes(cmd))) ? 'composing' : 'available';
+    const presenceStatus = commands.some((cmd) => (cmd instanceof RegExp ? cmd.test(m.text) : m.text.includes(cmd))) ? 'composing' : 'unavailable';
   
     if (presenceStatus) await this.sendPresenceUpdate(presenceStatus, m.chat);
   }
   
-  export const disabled = true;
+  export const disabled = false;
